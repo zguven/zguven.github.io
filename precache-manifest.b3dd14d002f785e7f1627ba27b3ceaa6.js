c91e6f96f09c10da0812d3212b53d7c4c4aaf0bb
Zülkif Güven <56037334+zguven@users.noreply.github.com>
2020-08-29 22:39:43 +0300

    Add files via upload

---
+++ precache-manifest.b3dd14d002f785e7f1627ba27b3ceaa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b26cbdc6df84849b62b61b5c68e790",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ba39a76ca0f5e70158",
+    "url": "/static/css/main.39003071.chunk.css"
+  },
+  {
+    "revision": "21888b1eaa1d9f551e94",
+    "url": "/static/js/2.aea71118.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.aea71118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ba39a76ca0f5e70158",
+    "url": "/static/js/main.48c1f67d.chunk.js"
+  },
+  {
+    "revision": "55dc2d6d8891bead9b41",
+    "url": "/static/js/runtime-main.31687faf.js"
+  }
+]);
